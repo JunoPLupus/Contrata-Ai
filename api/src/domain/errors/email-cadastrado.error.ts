7b334d23760d46011b783a4b0ed51fa2301605d5
Juno Lupus <junoplupus@gmail.com>
2026-05-14 14:22:04 -0300

    fix: Remove email-cadastrado.error.ts
    Classe removida por não ser mais necessária.

--- api/src/domain/errors/email-cadastrado.error.ts
+++
@@ -1,9 +0,0 @@
-import { DomainError } from "./domain.error";
-
-export class EmailJaCadastradoError extends DomainError {
-
-    constructor() {
-        super('EmailJaCadastradoError',
-            'E-mail indisponível.');
-    }
-}
