4a9ba8b96e7636677b84fbff02821365f95ed083
Juno Lupus <junoplupus@gmail.com>
2026-05-14 14:25:51 -0300

    feat: Cria models de usuario e prestador
    - Models e Schemas de Usuario e Prestador criados ;
    
    - UsuarioModel e PrestadorModel criados e exportados para serem utilizados nas implementações dos repositories de usuário e prestador.

---
+++ api/src/infra/models/usuario/usuario.model.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+interface IUsuarioDocument {
+    nome: string
+    email: string
+    senha: string
+    telefone?: string
+    whatsapp?: string
+    perfis: Array<'cliente' | 'prestador'>
+    localizacao_cidade?: string
+    localizacao_cep?: string
+    reputacao_flag_cancelamento: number
+    data_cadastro: Date
+    ativo: boolean
+}
+
+const usuarioSchema = new Schema<IUsuarioDocument>({
+    nome: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    senha: { type: String, required: true },
+    telefone: String,
+    whatsapp: String,
+    perfis: [{ type: String, enum: ['cliente', 'prestador'] }],
+    localizacao_cidade: String,
+    localizacao_cep: String,
+    reputacao_flag_cancelamento: { type: Number, default: 0 },
+    data_cadastro: { type: Date, default: Date.now },
+    ativo: { type: Boolean, default: true },
+})
+
+export const UsuarioModel = model('Usuario', usuarioSchema)
