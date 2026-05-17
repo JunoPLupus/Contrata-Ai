94eef9d07e8e640d2ea6832ab382d14c4896bca5
Juno Lupus <junoplupus@gmail.com>
2026-05-17 17:22:16 -0300

    refactor: Altera parâmetro de use-case de cadastro de prestador
    Para seguir o padrão de use-case para escritas no banco, o método execute do CadastrarPrestadorUseCase agora recebe um PrestadorCadastroDTO como parâmetro.

--- api/src/domain/use-cases/prestador/cadastrar-prestador/cadastrar-prestador.use-case.ts
+++ api/src/domain/use-cases/prestador/cadastrar-prestador/cadastrar-prestador.use-case.ts
@@ -1,12 +1,13 @@
 import { Prestador } from "../../../entities/prestador/prestador.entity";
 import { IPrestadorRepository } from "../../../repositories/prestador.repository";
 import { CampoObrigatorioVazioError } from "../../../errors/campo-obrigatorio-vazio.error";
+import { PrestadorCadastroDTO } from "../../../dto/prestador/prestador-cadastro.dto";
 
 export class CadastrarPrestadorUseCase {
     constructor(private readonly prestadorRepository: IPrestadorRepository) {}
 
-    async execute(idCliente: string): Promise<Prestador> {
-        if(this.isVazio(idCliente)) throw new CampoObrigatorioVazioError('idCliente')
+    async execute(prestadorDTO : PrestadorCadastroDTO): Promise<Prestador> {
+        if(this.isVazio(prestadorDTO.idCliente)) throw new CampoObrigatorioVazioError('idCliente')
 
         const propsPrestador = {
             idCliente: idCliente,
