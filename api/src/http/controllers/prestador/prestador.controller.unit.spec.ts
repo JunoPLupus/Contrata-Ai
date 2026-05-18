7a14fbcdb526070b1704691e6c234775bd04ac58
Juno Lupus <junoplupus@gmail.com>
2026-05-17 23:43:57 -0300

    feat: Adiciona testes unitários dos controllers de usuário e prestador
    - Testes de controllers focados apenas nas responsabilidades do controller, os retornos de erros são responsabilidade do middleware de erro e os testes dele.

--- api/src/http/controllers/prestador/prestador.controller.unit.spec.ts
+++ api/src/http/controllers/prestador/prestador.controller.unit.spec.ts
@@ -1,5 +1,37 @@
+import { Request, Response } from "express";
+
 import { PrestadorController } from "./prestador.controller";
+import { CadastrarPrestadorUseCase } from "../../../domain/use-cases/prestador/cadastrar-prestador/cadastrar-prestador.use-case";
+import { PrestadorMother } from "../../../test-helpers/prestador.mother";
 
 describe('Prestador Controller', () => {
     let controller : PrestadorController
+    let cadastrarPrestadorUseCaseMock : jest.Mocked<CadastrarPrestadorUseCase>
+    let req : Partial<Request>
+    let res : Partial<Response>
+
+    beforeEach( async () => {
+        cadastrarPrestadorUseCaseMock = { execute: jest.fn() } as any
+        controller = new PrestadorController(cadastrarPrestadorUseCaseMock)
+
+        req = { body: PrestadorMother.criarDTO() }
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+    })
+
+    it('deve retornar resposta com sucesso 201 e objeto prestadorRespostaCadastroDTO ao cadastrar', async () => {
+        // Arrange
+        const prestadorMock = PrestadorMother.criarValido(req.body)
+        cadastrarPrestadorUseCaseMock.execute.mockResolvedValue(prestadorMock)
+        // Act
+        await controller.cadastrar(req as any, res as any)
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(201)
+        expect(res.json).toHaveBeenCalledWith({
+            id: prestadorMock.id,
+            idCliente: prestadorMock.idCliente
+        })
+    })
 })
--- api/src/http/controllers/usuario/usuario.controller.unit.spec.ts
+++ api/src/http/controllers/usuario/usuario.controller.unit.spec.ts
@@ -1,5 +1,68 @@
+import { Request, Response } from 'express'
+
 import { UsuarioController } from "./usuario.controller";
+import { CadastrarUsuarioUseCase } from "../../../domain/use-cases/usuario/cadastrar-usuario/cadastrar-usuario.use-case";
+import { VerificarEmailUseCase } from "../../../domain/use-cases/usuario/verificar-email/verificar-email.use-case";
+import { Usuario } from "../../../domain/entities/usuario/usuario.entity";
+import { UsuarioMother } from "../../../test-helpers/usuario.mother";
 
 describe('UsuarioController', () => {
-    let controller: UsuarioController;
+    let controller: UsuarioController
+    let cadastrarUsuarioUseCaseMock: jest.Mocked<CadastrarUsuarioUseCase>
+    let verificarEmailUseCaseMock: jest.Mocked<VerificarEmailUseCase>
+    let req: Partial<Request>
+    let res: Partial<Response>
+    let usuarioMock : Usuario
+
+    beforeEach( async () => {
+        cadastrarUsuarioUseCaseMock = { execute: jest.fn() } as any
+        verificarEmailUseCaseMock = { execute: jest.fn() } as any
+        controller = new UsuarioController(cadastrarUsuarioUseCaseMock, verificarEmailUseCaseMock)
+
+        req = { body: UsuarioMother.criarDTOValido(), query: { email: "fulano@gmail.com" } }
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+        usuarioMock = UsuarioMother.criarUsuarioValido(req.body)
+    })
+
+    it('deve retornar resposta com sucesso 201 e objeto usuarioRespostaCadastroDTO ao cadastrar', async () => {
+        // Arrange
+        cadastrarUsuarioUseCaseMock.execute.mockResolvedValue(usuarioMock)
+        // Act
+        await controller.cadastrar(req as any, res as any)
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(201)
+        expect(res.json).toHaveBeenCalledWith({
+            id: usuarioMock.id,
+            nome: usuarioMock.nome,
+            email: usuarioMock.email,
+            perfis: usuarioMock.perfis
+        })
+    })
+
+    it('deve retornar resposta com sucesso 200 e objeto usuarioRespostaCadastroDTO ao buscar email válido', async () => {
+        // Arrange
+        verificarEmailUseCaseMock.execute.mockResolvedValue(usuarioMock)
+        // Act
+        await controller.buscarPorEmail(req as any, res as any)
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({
+            id: usuarioMock.id,
+            nome: usuarioMock.nome,
+            email: usuarioMock.email,
+            perfis: usuarioMock.perfis
+        })
+    })
+
+    it('deve retornar resposta com erro 404 ao buscar email não existente no banco', async () => {
+        // Arrange
+        verificarEmailUseCaseMock.execute.mockResolvedValue(null)
+        // Act
+        await controller.buscarPorEmail(req as any, res as any)
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(404)
+    })
 })
