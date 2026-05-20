45d9347710b55fa6e90eb491af835e32c052c40e
Juno Lupus <junoplupus@gmail.com>
2026-05-20 18:57:40 -0300

    feat: Protege rota de prestador com autenticação JWT
    - Controller de prestador agora busca o idCliente de req.user ao invés de req.body, os testes foram atualizados de acordo ;
    
    - Middleware ExigeAutenticacao foi adicionado na rota de cadastro de prestadores.
    
    Agora é necessário usar a rota de login para conseguir cadastrar um prestador.

--- api/src/http/routes/prestador/prestador.routes.ts
+++ api/src/http/routes/prestador/prestador.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { prestadorController } from "../../../shared/container";
+import { exigeAutenticacao } from "../../middlewares/exige-autenticacao/exige-autenticacao.middleware";
 
 const prestadorRouter = Router()
-prestadorRouter.post('/prestadores', (req, res) => prestadorController.cadastrar(req, res))
+prestadorRouter.post('/prestadores', exigeAutenticacao, (req, res) => prestadorController.cadastrar(req, res))
 
 export default prestadorRouter
