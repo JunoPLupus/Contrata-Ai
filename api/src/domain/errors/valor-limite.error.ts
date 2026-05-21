c10d2e03aec0acd6829c064285264b6af74eb373
Juno Lupus <junoplupus@gmail.com>
2026-05-21 18:29:26 -0300

    feat: Adiciona parâmetro opcional em ValorLimiteError
    Por padrão recebe isNumero false para exibir a mensagem de limite de string, caso seja true, vai exigir a mensagem de limite de number.

--- api/src/domain/errors/valor-limite.error.ts
+++ api/src/domain/errors/valor-limite.error.ts
@@ -3,8 +3,12 @@ import { DomainError } from "./domain.error";
 export class ValorLimiteError extends DomainError {
     readonly statusCode: number = 422
 
-    constructor(nomeCampo : string, limite : number, tipo: 'mínimo' | 'máximo') {
-        super('ValorLimiteError',
-            `O campo '${nomeCampo}' deve conter no ${tipo} ${limite} caracteres.`);
+    constructor(nomeCampo : string, limite : number, tipo: 'mínimo' | 'máximo', isNumero : boolean = false) {
+        let mensagem = `O campo '${nomeCampo}' `
+        mensagem = isNumero?
+            mensagem + `deve ser no ${tipo} ${limite}.` :
+            mensagem + `deve conter no ${tipo} ${limite} caracteres.`
+
+        super('ValorLimiteError', mensagem);
     }
 }
