7caf6507fabce76b6f7e6af8ecfd1a7dcf7d990e
Juno Lupus <junoplupus@gmail.com>
2026-05-21 17:23:35 -0300

    feat: Implementa métodos de verificação de number no Value Object Base

--- api/src/domain/value-objects/value-object.base.ts
+++ api/src/domain/value-objects/value-object.base.ts
@@ -1,4 +1,10 @@
-import { isAbaixoLimite, isAcimaLimite, isString, isStringVazia } from "../utils/value-objects.utils";
+import {
+    isStringAbaixoLimite,
+    isStringAcimaLimite,
+    isString,
+    isStringVazia,
+    isNumber, isNumberAbaixoLimite, isNumberAcimaLimite
+} from "../utils/value-objects.utils";
 import { CampoObrigatorioVazioError } from "../errors/campo-obrigatorio-vazio.error";
 import { FormatoInvalidoError } from "../errors/formato-invalido.error";
 import { ValorLimiteError } from "../errors/valor-limite.error";
@@ -17,7 +23,13 @@ export abstract class ValueObjectBase {
     protected validarString(value : any) : void {
         if (isStringVazia(value)) throw new CampoObrigatorioVazioError(this._campo)
         else if (!isString(value)) throw new FormatoInvalidoError(this._campo)
-        else if (this._limiteMinimo > 0 && isAbaixoLimite(value, this._limiteMinimo)) throw new ValorLimiteError(this._campo, this._limiteMinimo, 'mínimo')
-        else if (this._limiteMaximo > 0 && isAcimaLimite(value, this._limiteMaximo)) throw new ValorLimiteError(this._campo, this._limiteMaximo, 'máximo')
+        else if (this._limiteMinimo > 0 && isStringAbaixoLimite(value, this._limiteMinimo)) throw new ValorLimiteError(this._campo, this._limiteMinimo, 'mínimo')
+        else if (this._limiteMaximo > 0 && isStringAcimaLimite(value, this._limiteMaximo)) throw new ValorLimiteError(this._campo, this._limiteMaximo, 'máximo')
+    }
+
+    protected validarNumber(value : any) : void {
+        if(!isNumber(value)) throw new FormatoInvalidoError(this._campo)
+        else if (this._limiteMinimo > 0 && isNumberAbaixoLimite(value, this._limiteMinimo)) throw new ValorLimiteError(this._campo, this._limiteMinimo, 'mínimo')
+        else if (this._limiteMaximo > 0 && isNumberAcimaLimite(value, this._limiteMaximo)) throw new ValorLimiteError(this._campo, this._limiteMaximo, 'máximo')
     }
 }
