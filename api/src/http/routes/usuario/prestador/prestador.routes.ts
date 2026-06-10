b7498c82afe24fd4da332ba945ed4573e19cb5e9
Juno Lupus <junoplupus@gmail.com>
2026-06-10 16:24:48 -0300

    refactor: Renomeia arquivos e move diretórios das rotas, e separa o UsuarioRouter em dois
    - Cria ClienteRouter que possui a rota de cadastrar cliente que antes pertencia a UsuarioRouter.

--- api/src/http/routes/usuario/prestador/prestador.routes.ts
+++ api/src/http/routes/usuario/prestador/prestador.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { prestadorController } from "../../../shared/container";
-import { exigeAutenticacao } from "../../middlewares/exige-autenticacao/exige-autenticacao.middleware";
+import { prestadorController } from "../../../../shared/container";
+import { exigeAutenticacao } from "../../../middlewares/exige-autenticacao/exige-autenticacao.middleware";
 
 const prestadorRouter = Router()
 prestadorRouter.post('/prestadores', exigeAutenticacao, (req, res) => prestadorController.cadastrar(req, res))
