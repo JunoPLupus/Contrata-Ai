d83ce5906a7e0cd8f4b37e3ec5e06ca9743fd79b
Efrasnn <efrasnascimento1998@gmail.com>
2026-07-02 20:53:20 -0300

    feat(prestador): adiciona logout, mudar perfil e empty state no hub #2

--- web/src/app/shared/layouts/logado/logado-layout.component.ts
+++ web/src/app/shared/layouts/logado/logado-layout.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderLogadoComponent } from '../../components/header-logado/header-logado.component';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-logado-layout',
@@ -10,4 +11,12 @@ import { HeaderLogadoComponent } from '../../components/header-logado/header-log
   templateUrl: './logado-layout.component.html',
   styleUrl: './logado-layout.component.scss',
 })
-export class LogadoLayoutComponent {}
+export class LogadoLayoutComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
