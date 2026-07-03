26eb7a21d28bb3edebb89bbc90579972f413cf6e
Efrasnn <efrasnascimento1998@gmail.com>
2026-07-02 21:07:50 -0300

    fix(layout): adiciona RouterLink no logado-layout #2

--- web/src/app/shared/layouts/logado/logado-layout.component.ts
+++ web/src/app/shared/layouts/logado/logado-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderLogadoComponent } from '../../components/header-logado/header-logado.component';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-logado-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderLogadoComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, HeaderLogadoComponent],
   templateUrl: './logado-layout.component.html',
   styleUrl: './logado-layout.component.scss',
 })
